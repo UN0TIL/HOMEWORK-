4532cf561cb10a06f0ae28255944ab8c69bee523
UN0TIL <roberschool007@gmail.com>
2024-02-29 21:50:01 +0200

    add hw12

---
+++ homework_12.py
@@ -0,0 +1,10 @@
+from string import punctuation
+
+text = input("Введи текст: ")
+text = text.title()
+text = text.replace(" ", "")
+text = ''.join(char for char in text if char not in punctuation)
+if len(text) > 140:
+    x = len(text) - 140
+    text = text[:-x]
+print("#" + text)

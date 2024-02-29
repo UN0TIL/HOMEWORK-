f7eca87df98001c43f9a3fc9aa9224074a5cdecf
UN0TIL <roberschool007@gmail.com>
2024-02-29 21:54:48 +0200

    change hw12

--- homework_12.py
+++ homework_12.py
@@ -5,6 +5,6 @@ text = text.title()
 text = text.replace(" ", "")
 text = ''.join(char for char in text if char not in punctuation)
 if len(text) > 140:
-    x = len(text) - 140
+    x = len(text) - 139
     text = text[:-x]
-print("#" + text)
+print(len("#" + text))

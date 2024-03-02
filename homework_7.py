e53804f1b912288a8e72296ff905f734f3dede5a
UN0TIL <roberschool007@gmail.com>
2024-03-02 22:15:54 +0200

    change hw7

--- homework_7.py
+++ homework_7.py
@@ -1,14 +1,7 @@
-lane = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-for i in range(len(lane)):
-    if lane[i] == 0:
-        lane.insert(-1, lane[i])
-        lane.pop(i)
-print(lane)
-
-
-lane = list(input())
-for i in range(len(lane)):
-    if lane[i] == "0":
-        lane.extend(lane[i])
-        lane.pop(i)
-print(lane)
+a = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+n = len(a)
+for i in range(n - 1):
+    for j in range(n - 1 - i):
+        if a[j] == 0:                  
+            a[j], a[j + 1] = a[j + 1], a[j]
+print(a)

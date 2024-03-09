1d2595b505479c68a1014cf03a3e46000b196c76
UN0TIL <roberschool007@gmail.com>
2024-03-09 23:31:12 +0200

    add hw16

---
+++ homework_16.py
@@ -0,0 +1,4 @@
+name, old = input('Напишите ваше имя: '), int(input('Укажите ваш возвраст: '))
+def say_hi(name, old):
+    return print(f"Hi. My nane is {name} and I'm {old} years old")
+say_hi(name, old)
